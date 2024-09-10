1faf70faa101260af7a9b072fa889d7acce5b2c6
ViniFOL <viniihfernandes@gmail.com>
2024-09-09 23:58:50 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -16,5 +16,95 @@ let dados = [
         descricao: "Beatriz Souza é uma medalhista olímpica brasileira de judô. Sua força e técnica impecável a consagraram como uma das principais atletas da modalidade no país.",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
         tags: "judo judô judoka judoca"
-    }
+    },
+    {
+        titulo: "Alison dos Santos",
+        descricao: "O atleta brasileiro quebrou recordes mundiais nos 400m com barreiras, consolidando-se como uma das maiores promessas do atletismo mundial.",
+        link: "https://pt.wikipedia.org/wiki/Alison_dos_Santos",
+        tags: "atletismo 400m barreiras bronze"
+        },
+        
+        {
+        titulo: "Edival Pontes",
+        descricao: "O taekwondista brasileiro conquistou uma medalha histórica para o país, mostrando sua habilidade e determinação nas competições.",
+        link: "https://pt.wikipedia.org/wiki/Edival_Pontes",
+        tags: "taekwondo -68kg bronze"
+        },
+        
+        {
+        titulo: "Augusto Akio",
+        descricao: "O skatista brasileiro impressionou o mundo com suas manobras radicais e estilo único, garantindo um lugar no pódio.",
+        link: "https://pt.wikipedia.org/wiki/Augusto_Akio",
+        tags: "skate park bronze"
+        },
+        
+        {
+        titulo: "Gabriel Medina",
+        descricao: "O surfista brasileiro, já consagrado mundialmente, mais uma vez mostrou sua maestria nas ondas, conquistando uma nova medalha olímpica.",
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+        tags: "surfe surf surfista bronze"
+        },
+        
+        {
+        titulo: "Beatriz Ferreira",
+        descricao: "A boxeadora brasileira dominou o ringue e conquistou uma medalha histórica para o boxe feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Ferreira",
+        tags: " bia boxe 60kg bronze"
+        },
+        
+        {
+        titulo: "Flávia Saraiva",
+        descricao: "A ginasta brasileira, conhecida por sua elegância e dificuldade, foi fundamental para a conquista da medalha da equipe.",
+        link: "https://pt.wikipedia.org/wiki/Flávia_Saraiva",
+        tags: "ginasta flavia ginástica equipe bronze"
+        },
+        
+        {
+        titulo: "Larissa Pimenta",
+        descricao: "A judoca brasileira mostrou sua força e técnica, garantindo uma medalha importante para o judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Larissa_Pimenta",
+        tags: "judô judo judoca judoka -52kg bronze"
+        },
+        
+        {
+        titulo: "Isaquias Queiroz",
+        descricao: "O canoísta brasileiro, já multicampeão mundial, mais uma vez dominou sua categoria, mostrando sua força e experiência.",
+        link: "https://pt.wikipedia.org/wiki/Isaquias_Queiroz",
+        tags: "canoagem 1000m prata canoa"
+        },
+        
+        {
+        titulo: "Tatiana Weston-Webb",
+        descricao: "A surfista brasileira, com sua técnica impecável, conquistou uma medalha histórica para o surfe feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb",
+        tags: "surf surfe weston webb prata"
+        },
+        
+        {
+        titulo: "Willian Lima",
+        descricao: "O judoca brasileiro mostrou sua habilidade e determinação, garantindo uma medalha importante para o judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Willian_Lima",
+        tags: "judô judo judoca judoka -66kg prata"
+        },
+        
+        {
+        titulo: "Caio Bonfim",
+        descricao: "O atleta brasileiro, com sua resistência e técnica, conquistou uma medalha histórica para a marcha atlética brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Caio_Bonfim",
+        tags: "atletismo marcha atlética atletica 20km prata"
+        },
+        
+        {
+        titulo: "Ana Patrícia",
+        descricao: "A atleta brasileira, metade da dupla que dominou o vôlei de praia, é conhecida por sua força e habilidade no bloqueio.",
+        link: "https://pt.wikipedia.org/wiki/Ana_Patrícia_Ramos",
+        tags: "vôlei volei praia ouro patricia"
+        },
+        
+        {
+        titulo: "Duda Lisboa",
+        descricao: "A atleta brasileira, metade da dupla que dominou o vôlei de praia, é conhecida por sua versatilidade e habilidade no ataque.",
+        link: "https://pt.wikipedia.org/wiki/Duda Lisboa",
+        tags: "vôlei volei praia ouro"
+        }
 ];
