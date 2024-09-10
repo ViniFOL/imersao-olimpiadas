261278671aec2444e76ce8dfb6c1df24d22cbd3e
ViniFOL <viniihfernandes@gmail.com>
2024-09-09 22:19:01 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma das maiores promessas do skate mundial. A brasileira já conquistou diversos títulos e inspira milhões de jovens com seu talento e carisma.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skte fata sk8"
+    }, 
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é um nome que brilha no cenário da ginástica artística mundial. A brasileira, com sua força, agilidade e determinação, conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginasta salto ginastica ouro"
+    }, 
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma medalhista olímpica brasileira de judô. Sua força e técnica impecável a consagraram como uma das principais atletas da modalidade no país.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoka judoca"
+    }
+];
